a5026769cf94bb9151630c0aa8430c5c413a2c89
Alvaro Cabrera <alvaro7mrz@gmail.com>
2021-09-14 21:15:11 -0500

    FichaRegistroMedico

---
+++ src/main/java/pe/edu/upc/entity/Ficha_Registro_Medico.java
@@ -0,0 +1,25 @@
+package pe.edu.upc.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Ficha_Registro_Medico")
+public class Ficha_Registro_Medico implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	
+
+}
