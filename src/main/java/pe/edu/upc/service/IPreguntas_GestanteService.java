96bb40513e1508d0b8106cb322c30c33917697a9
mleia78 <70586791+Pardito123@users.noreply.github.com>
2021-09-20 22:56:03 -0500

    10.55SundoCambio

--- src/main/java/pe/edu/upc/service/IPreguntas_GestanteService.java
+++ src/main/java/pe/edu/upc/service/IPreguntas_GestanteService.java
@@ -7,5 +7,5 @@ import pe.edu.upc.entity.Preguntas_Gestante;
 public interface IPreguntas_GestanteService {
 	public void insertar(Preguntas_Gestante preguntas_Gestante);
 	public List<Preguntas_Gestante> listar();
-	public void eliminar(int idPreguntas_Gestante);
+	public void eliminar(int idPreguntas_Gestante, int IDPregunta);
 }
